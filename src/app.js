fdec74a94e7fd4a2e645c9ec8ad0c09bfda8a69a
Muhammad Mamooji <mamoojim@hotmail.com>
2021-01-06 11:58:28 -0500

    changed header title for index

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.static(publicDirectoryPath));
 //routes
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather",
+    title: "Weather Search",
     name: "Muhammad Mamooji",
   });
 });
